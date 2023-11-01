66902db8289c481b19c9d7396421e7e2979dfbd3
Christopher Ojaide <christopher.ojaide@gmail.com>
2023-11-01 11:42:43 +0100

    Initial commit

---
+++ artnet/src/main/java/app/artnet/ArtnetApplication.java
@@ -0,0 +1,13 @@
+package app.artnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtnetApplication.class, args);
+	}
+
+}
---
+++ artnet/src/test/java/app/artnet/ArtnetApplicationTests.java
@@ -0,0 +1,13 @@
+package app.artnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
