5d4c354feca1628b2915e8f5f6ed72c350c4ee7f
Christopher Ojaide <christopher.ojaide@gmail.com>
2023-11-06 10:40:42 +0100

    Added code to PostService and CommentService.

--- artnet-client/src/services/CommentService.js
+++ artnet-client/src/services/CommentService.js
@@ -1,10 +1,29 @@
 import axios from 'axios';
 
-
 const COMMENT_BASE_URL =  "http://localhost:8080/api/v1/comment";
                       
+
 class CommentService{
-    //...
+    
+    createComment(comment){
+        return axios.post(COMMENT_BASE_URL + '/save', comment);
+    }
+
+    getAllByUserId(userId){
+        return axios.get(COMMENT_BASE_URL + '/get-all-by-user-id/' + userId);
+    }
+
+    getAllByPostId(postId){
+        return axios.get(COMMENT_BASE_URL + '/get-all-by-post-id/' + postId);
+    }
+
+    deleteComment(commentId){
+        return axios.delete(COMMENT_BASE_URL + '/delete/' + commentId);
+    }
+
+    deleteAllCommentByUserId(userId){
+        return axios.delete(COMMENT_BASE_URL + '/delete-all-by-id/' + userId);
+    }
 }
 
 export default new CommentService()
--- artnet-client/src/services/PostService.js
+++ artnet-client/src/services/PostService.js
@@ -1,10 +1,41 @@
 import axios from 'axios';
 
-
 const POST_BASE_URL = "http://localhost:8080/api/v1/post";
                       
+
 class PostService{
-    //...
+    
+    createPost(post){
+        return axios.post(POST_BASE_URL + '/save', post);
+    }
+
+    getAllPost(){
+        return axios.get(POST_BASE_URL + '/get-all');
+    }
+
+    getAllByUserId(userId){
+        return axios.get(POST_BASE_URL + '/get-all-by-user-id/' + userId);
+    }
+
+    getPostByPostId(postId){
+        return axios.get(POST_BASE_URL + '/get-by-postId/' + postId);
+    }
+
+    setLikes(postId, localUserId){
+        return axios.put(POST_BASE_URL + '/likes/' + postId + '/' + localUserId);
+    }
+
+    updatePost(postData){
+        return axios.put(POST_BASE_URL + '/update', postData);
+    }
+
+    deletePost(postId){
+        return axios.delete(POST_BASE_URL + '/delete/' + postId);
+    }
+
+    deleteAllPostByUserId(userId){
+        return axios.delete(POST_BASE_URL + '/delete-all-by-id/' + userId);
+    }
 }
 
 export default new PostService()
