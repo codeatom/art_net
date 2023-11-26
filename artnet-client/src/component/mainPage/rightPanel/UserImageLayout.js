b330369c4db420a31ce51a4cd09085d21f08ad4c
Christopher Ojaide <christopher.ojaide@gmail.com>
2023-11-26 16:51:06 +0100

    Added code for displaying a selected user posts.

--- artnet-client/src/component/mainPage/rightPanel/UserImageLayout.js
+++ artnet-client/src/component/mainPage/rightPanel/UserImageLayout.js
@@ -1,7 +1,14 @@
+import { useDispatch } from 'react-redux';
+import PostService from '../../../services/PostService';
+import UserService from '../../../services/UserService';
 import avatar from "../../../images/avatar.png";
+
+import { updateUserPostArray } from '../../../store/storeUtil';
+import { setIsPostDetail } from '../../../store/storeUtil';
 
 
 const ImageLayout = (props) => {
+    const dispatch = useDispatch();
 
     const handleUserImg = (profileImg) => {
         if (profileImg == "" || profileImg == undefined) {
@@ -10,13 +17,44 @@ const ImageLayout = (props) => {
         return profileImg;
     }
 
+    const getAllByUserId = (userId) => {
+        PostService.getAllByUserId(userId).then(response => {
+            if (response.data.length > 0) {
+                updateUserPostArray(dispatch, response.data);
+            } else {
+                getDummyPostArray(userId);
+            }
+        });
+
+        setIsPostDetail(dispatch, false);
+    }
+
+    const getDummyPostArray = (userId) => {
+        UserService.getUserByUserId(userId).then(response => {
+            let dummyPostArray = [
+                {
+                    postId: 0,
+                    description: response.data.userName + " " + "has no post yet",
+                    postImgURL: "",
+                    postDate: "",
+                    likeArray: [],
+                    user: response.data,
+                    comments: []
+                }
+            ]
+            updateUserPostArray(dispatch, dummyPostArray);
+        });
+    }
+
+
+
     return (
-        <div className='user-img-layout bg-col-chngr' key={props.item.userId}>
-            <div className='user-img'>
+        <div className='user-img-layout bg-col-chngr'>
+            <div className='user-img' key={props.item.userId}>
                 <div>
                     <img className='avatar' src={handleUserImg(props.item.userImage)} />
                 </div>
-                <div className='user-name txt'>
+                <div className='user-name txt' onClick={() => getAllByUserId(props.item.userId)}>
                     {props.item.userName}
                 </div>
             </div>
